622e632a13eb330e156d843f75df7a218a869e09
yahir140306 <joseyahirhernandez11@gmail.com>
2025-07-14 21:49:28 -0600

    feat: add DashboardPageComponent with template and initial structure

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [],
+  templateUrl: `./dashboard-page.component.html`,
+})
+export default class DashboardPageComponent {}
