97489fb557eb428fb0d212aacf44dbddb3c5fe5a
yahir140306 <joseyahirhernandez11@gmail.com>
2025-07-14 21:49:22 -0600

    feat: create SearchPageComponent with initial template and structure

---
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page',
+  imports: [],
+  templateUrl: './search-page.component.html',
+})
+export default class SearchPageComponent {}
