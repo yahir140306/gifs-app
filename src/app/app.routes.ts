272c4b8891427a0be9fa9b98566e3c8576feb388
yahir140306 <joseyahirhernandez11@gmail.com>
2025-07-14 21:49:34 -0600

    feat: define application routes for dashboard, trending, and search pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      // import('./gifs/pages/dashboard-page/dashboard-page.component').then(
+      //   (c) => c.DashboardPageComponent
+      // ),
+      import('./gifs/pages/dashboard-page/dashboard-page.component'),
+  },
+  {
+    path: 'trending',
+    loadComponent: () =>
+      import('./gifs/pages/trending-page/trending-page.component'),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./gifs/pages/search-page/search-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
