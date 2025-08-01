1ebb4ed610cb211c800732e64b556bacbc5556cd
yahir140306 <joseyahirhernandez11@gmail.com>
2025-07-31 23:12:53 -0600

    feat: update dashboard page layout and integrate router outlet

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: `./dashboard-page.component.html`,
 })
 export default class DashboardPageComponent {}
