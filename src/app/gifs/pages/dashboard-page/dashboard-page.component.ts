04e8a3272c0e5500e6c0474df343ea11d8ee5574
yahir140306 <joseyahirhernandez11@gmail.com>
2025-08-03 18:19:38 -0600

    feat: integrate side menu component into dashboard page

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuComponent],
   templateUrl: `./dashboard-page.component.html`,
 })
 export default class DashboardPageComponent {}
