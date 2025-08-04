34da01f9b3e5de86b9e3e3da3ffabfa3ad2476bb
yahir140306 <joseyahirhernandez11@gmail.com>
2025-08-03 18:19:28 -0600

    feat: add side menu header component with logo and user profile

---
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'gifs-side-menu-header',
+  imports: [],
+  templateUrl: './side-menu-header.component.html',
+})
+export class SideMenuHeaderComponent {}
