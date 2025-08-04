8c95d75a3fabe014d529ae6cbe0088478ca9f208
yahir140306 <joseyahirhernandez11@gmail.com>
2025-08-03 18:19:33 -0600

    feat: add side menu options component with navigation links

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [],
+  templateUrl: './side-menu-options.component.html',
+})
+export class SideMenuOptionsComponent {}
